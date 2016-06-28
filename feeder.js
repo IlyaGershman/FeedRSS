d13327a5161bbd70e07eb189c20d9d1bfc990275
IlyaGershman <ilgershman@gmail.com>
2016-06-28 10:54:54 +0300

    Update feeder.js

--- feeder.js
+++ feeder.js
@@ -73,7 +73,7 @@ App.controller("FeedCtrl", ['$scope','$location', 'FeedService', function ($scop
 App.factory('FeedService',['$http',function($http){
     return {
         parseFeed : function(url){
-            return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+            return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
         }
     }
 }]);
